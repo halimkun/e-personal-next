d531d4f7f0ca3d1697362cf47efbac22e268e6cd
FAISAL HALIM <ffaisalhalim@gmail.com>
2023-11-23 22:21:35 +0700

    Add theme provider component and update app component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,9 @@ import '@/styles/globals.css'
 import type { ReactElement, ReactNode } from 'react'
 import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
+import { useTheme } from 'next-themes'
 import { AuthProvider } from '@/context/auth-context'
+import { ThemeProvider } from '@/components/theme-provider'
 import NextNProgress from 'nextjs-progressbar';
 
 
@@ -16,11 +18,13 @@ type AppPropsWithLayout = AppProps & {
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page)
-  return getLayout(
-    <AuthProvider>
-      <NextNProgress />
-      <Component {...pageProps} />
-    </AuthProvider>
+  return (
+    <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+      <AuthProvider>
+        <NextNProgress />
+        {getLayout(<Component {...pageProps} />)}
+      </AuthProvider>
+    </ThemeProvider>
   )
 }
 
