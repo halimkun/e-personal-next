224d296854f3ef267869472aa6eae283ac7098c6
FAISAL HALIM <ffaisalhalim@gmail.com>
2023-12-08 08:02:23 +0700

    feat perjanjian kerjasama now support pks innternal and eksternal with automatic letter numberring

--- src/pages/berkas/kerjasama.tsx
+++ src/pages/berkas/kerjasama.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, useState } from "react";
+import { ReactElement, useEffect, useState } from "react";
 import { NextPageWithLayout } from "../_app";
 
 import AppLayout from "@/components/layouts/app";
@@ -7,7 +7,7 @@ import LaravelPagination from "@/components/custom/tables/laravel-pagination";
 import { getDate, getFullDate } from "@/lib/date";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import { IconDotsVertical, IconEdit, IconFileSearch, IconInnerShadowTop, IconPlus, IconTrash } from "@tabler/icons-react";
+import { IconDotsVertical, IconEdit, IconFileSearch, IconInnerShadowTop, IconPlus, IconSettings2, IconTrash } from "@tabler/icons-react";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip"
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
@@ -17,6 +17,8 @@ import { Combobox } from "@/components/custom/inputs/combo-box";
 import { getSession } from "next-auth/react";
 import { toast } from "react-hot-toast";
 import { useRouter } from "next/router";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+
 
 
 const BerkasKerjasama: NextPageWithLayout = () => {
@@ -27,7 +29,63 @@ const BerkasKerjasama: NextPageWithLayout = () => {
   const [isPreview, setIsPreview] = useState(false);
   const [isEdit, setIsEdit] = useState(false);
   const [pks, setPks] = useState<any>([]);
+
   const router = useRouter();
+
+  const [lastNomor, setLastNomor] = useState<any>({
+    internal: null,
+    eksternal: null
+  });
+
+  const [selectedType, setSelectedType] = useState<'internal' | 'eksternal'>('internal');
+  const [tanggal, setTanggal] = useState<string | undefined>(new Date().toISOString().slice(0, 10));
+  const [noPksInternal, setNoPksInternal] = useState<string | undefined>('');
+  
+  useEffect(() => {
+    if (lastNomor['internal'] && lastNomor['eksternal']) {
+      parseNomorSurat()
+    }
+  }, [lastNomor, selectedType, tanggal])
+  
+  useEffect(() => {
+    const fetchLastNomor = async () => {
+      const session = await getSession();
+      const res = await fetch(`https://sim.rsiaaisyiyah.com/rsiap-api-dev/api/berkas/pks/last-nomor`, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${session?.rsiap?.access_token}`
+        }
+      });
+      const result = await res.json();
+      if (result.success) {
+        setLastNomor(result.data)
+      } else {
+        console.log(result);
+      }
+    }
+
+    fetchLastNomor()
+  }, [])
+  
+  const parseNomorSurat = () => {
+    // Nomor Surat
+    const split = lastNomor[selectedType].split('/');
+    const n = parseInt(split[0]) + (isEdit ? 0 : 1);
+    console.log(n);
+    const nn = n < 100 ? `0${n}` : n;
+
+    // type surat
+    const t = selectedType == 'internal' ? 'A' : 'B';
+
+    // Tanggala
+    const tanggalSplit = tanggal?.split('-');
+    const tanggalParsed = tanggalSplit?.map((item, index) => { index == 0 ? item = item.slice(2) : item = item; return item }).reverse().join('');
+
+    // Merge All
+    const nomorSurat = `${nn}/${t}/PKS-RSIA/${tanggalParsed}`;
+    setNoPksInternal(nomorSurat);
+  }
+
 
   const onSubmit = async (e: any) => {
     e.preventDefault();
@@ -70,7 +128,7 @@ const BerkasKerjasama: NextPageWithLayout = () => {
       },
       body: updateData
     });
-    
+
     const result = await res.json();
 
     if (result.success) {
@@ -166,6 +224,7 @@ const BerkasKerjasama: NextPageWithLayout = () => {
               <DialogTrigger asChild>
                 <Button variant="default" size="icon" className="w-7 h-7" onClick={() => {
                   setPks([])
+                  parseNomorSurat()
                   setIsEdit(false)
                   setIsOpen(true)
                 }}>
@@ -175,40 +234,61 @@ const BerkasKerjasama: NextPageWithLayout = () => {
               </DialogTrigger>
               <DialogContent className="max-w-xl">
                 <DialogHeader>
-                  <DialogTitle>Entri Data Perjanjian Kerjasama</DialogTitle>
+                  <DialogTitle className="text-primary">Entri Data Perjanjian Kerjasama</DialogTitle>
                   <DialogDescription>Tambahkan data perjanjian kerjasama baru dengan mengisi form dibawah ini.</DialogDescription>
                 </DialogHeader>
                 {/* dialog content */}
                 <form method="post" encType="multipart/form-data" onSubmit={isEdit ? onUpload : onSubmit}>
                   {isEdit ? (<div className="mb-4 space-y-1.5">
-                    <Label htmlFor="id">Id Berkas</Label>
+                    <Label className="text-primary font-semibold" htmlFor="id">Id Berkas</Label>
                     <Input type="text" id="id" name="id" placeholder="id berkas" defaultValue={isEdit ? pks.id : ''} readOnly={true} />
                   </div>) : (<></>)}
                   <div className="mb-4 space-y-1.5">
-                    <Label htmlFor="judul">Judul</Label>
+                    <Label className="text-primary font-semibold" htmlFor="judul">Judul</Label>
                     <Input type="text" id="judul" name="judul" placeholder="Judul / Nama berkas" defaultValue={isEdit ? pks.judul : ''} />
                   </div>
-                  <div className="grid grid-cols-2 gap-3">
-                    <div className="mb-4 space-y-1.5">
-                      <Label htmlFor="no_pks_internal">No. PKS Internal</Label>
-                      <Input type="text" id="no_pks_internal" name="no_pks_internal" placeholder="No. PKS Internal" defaultValue={isEdit ? pks.no_pks_internal : ''} />
-                    </div>
-                    <div className="mb-4 space-y-1.5">
-                      <Label htmlFor="no_pks_eksternal">No. PKS Eksternal</Label>
-                      <Input type="text" id="no_pks_eksternal" name="no_pks_eksternal" placeholder="No. PKS Eksternal" defaultValue={isEdit ? pks.no_pks_eksternal : ''} />
-                    </div>
+
+                  <div className="mb-4 space-y-1.5 relative">
+                    <Label className="text-primary font-semibold" htmlFor="tipe-surat">Tipe Surat</Label>
+                    <RadioGroup className="flex gap-6" onValueChange={setSelectedType as any} defaultValue="internal">
+                      <div className="flex items-center space-x-2">
+                        <RadioGroupItem value="internal" id="r1" />
+                        <Label className="font-medium" htmlFor="r1">Internal Rumah Sakit</Label>
+                      </div>
+                      <div className="flex items-center space-x-2">
+                        <RadioGroupItem value="eksternal" id="r2" />
+                        <Label className="font-medium" htmlFor="r2">Antar Instansi</Label>
+                      </div>
+                    </RadioGroup>
+                  </div>
+
+                  <div className="grid grid-cols-2 gap-3 gap-y-1">
                     {/* tanggal awal dan akhir */}
                     <div className="mb-4 space-y-1.5">
-                      <Label htmlFor="tanggal_awal">Tanggal Awal</Label>
-                      <Input type="date" id="tanggal_awal" name="tanggal_awal" placeholder="Tanggal Awal" defaultValue={isEdit ? pks.tanggal_awal : ''} />
+                      <Label className="text-primary font-semibold" htmlFor="tanggal_awal">Tanggal Awal</Label>
+                      {/* get date now yyyy-MM-dd */}
+                      <Input type="date" id="tanggal_awal" name="tanggal_awal" placeholder="Tanggal Awal" defaultValue={isEdit ? pks.tanggal_awal : new Date().toISOString().slice(0, 10)} onChange={(e) => {
+                        setTanggal(e.currentTarget.value)
+                      }} />
                     </div>
                     <div className="mb-4 space-y-1.5">
-                      <Label htmlFor="tanggal_akhir">Tanggal Ahir</Label>
+                      <Label className="text-primary font-semibold" htmlFor="tanggal_akhir">Tanggal Ahir</Label>
                       <Input type="date" id="tanggal_akhir" name="tanggal_akhir" placeholder="Tanggal Ahir" defaultValue={isEdit ? pks.tanggal_akhir : ''} />
+                    </div>
+                    {/* No Pks Intenal Dan Eksternal */}
+                    <div className="mb-4 space-y-1.5">
+                      <Label className="text-primary font-semibold" htmlFor="no_pks_internal">No. PKS Internal</Label>
+                      <Input type="text" id="no_pks_internal" name="no_pks_internal" placeholder="No. PKS Internal" value={noPksInternal} readOnly onChange={(e) => {
+                        setNoPksInternal(e.currentTarget.value)
+                      }} />
+                    </div>
+                    <div className="mb-4 space-y-1.5">
+                      <Label className="text-primary font-semibold" htmlFor="no_pks_eksternal">No. PKS Eksternal</Label>
+                      <Input type="text" id="no_pks_eksternal" name="no_pks_eksternal" placeholder="No. PKS Eksternal" defaultValue={isEdit ? pks.no_pks_eksternal : ''} />
                     </div>
                   </div>
                   <div className="mb-4 space-y-1.5">
-                    <Label htmlFor="pj">Penanggung Jawab</Label>
+                    <Label className="text-primary font-semibold" htmlFor="pj">Penanggung Jawab</Label>
                     <Input type="hidden" id="pj" name="pj" defaultValue={selected} />
                     <Combobox items={[
                       { label: "Kicky Eka Shelviani, M.S.M", value: "3.925.0123" },
@@ -219,7 +299,7 @@ const BerkasKerjasama: NextPageWithLayout = () => {
                   </div>
                   {!isEdit ? (
                     <div className="mb-4 space-y-1.5">
-                      <Label htmlFor="file">File</Label>
+                      <Label className="text-primary font-semibold" htmlFor="file">File</Label>
                       <Input type="file" id="file" name="file" placeholder="File" />
                     </div>
                   ) : (<div className="mb-4"></div>)}
@@ -243,6 +323,7 @@ const BerkasKerjasama: NextPageWithLayout = () => {
             columns={pksColumns}
             onRowClick={(row: any) => {
               setPks(row)
+              setNoPksInternal(row.no_pks_internal)
               setIsRowClick(true)
             }}
             dataSrc={"https://sim.rsiaaisyiyah.com/rsiap-api-dev/api/berkas/pks"}
