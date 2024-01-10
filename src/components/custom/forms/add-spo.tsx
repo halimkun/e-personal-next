af7e8a7aaf737d3ac1e3a2cefe47516e3a2c09d7
FAISAL HALIM <ffaisalhalim@gmail.com>
2024-01-10 08:17:22 +0700

    Remove unused imports in add-spo.tsx

--- src/components/custom/forms/add-spo.tsx
+++ src/components/custom/forms/add-spo.tsx
@@ -7,10 +7,8 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Button } from "@/components/ui/button";
-import { Combobox } from "../inputs/combo-box";
 import { MultiSelect } from "../inputs/multi-select";
 import { Toggle } from "@radix-ui/react-toggle";
-import { IconCursorText } from "@tabler/icons-react";
 
 interface FormAddSpoProps {
   lastNomor: any
