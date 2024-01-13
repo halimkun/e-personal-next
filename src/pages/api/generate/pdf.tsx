94ca1c9007cbbd0fd90b65b74f3fda97593a80ea
FAISAL HALIM <ffaisalhalim@gmail.com>
2024-01-13 13:24:04 +0700

    Add PDF generation API endpoint and update SPO rendering page

---
+++ src/pages/api/generate/pdf.tsx
@@ -0,0 +1,40 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import puppeteer from 'puppeteer';
+
+
+const saveAsPdf = async (url: string, token?: string) => {
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    headless: 'new'
+  });
+  const page = await browser.newPage();
+
+  const urlToken = `${url}?token=${token}`
+
+  await page.goto(urlToken, {
+    waitUntil: 'networkidle0',
+  });
+
+  const result = await page.pdf({
+    format: 'a4',
+  });
+  await browser.close();
+
+  return result;
+};
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { url, token } = req.query; // pass the page to create PDF from as param
+
+  // get data dulu disini baru di pass ke saveAsPdf sebagai url params
+
+  res.setHeader(
+    'Content-Disposition',
+    `attachment; filename="file.pdf"`
+  );
+  res.setHeader('Content-Type', 'application/pdf');
+
+  const pdf = await saveAsPdf(url as string, token as string);
+
+  return res.send(pdf);
+};
--- src/pages/berkas/spo/render.tsx
+++ src/pages/berkas/spo/render.tsx
@@ -1,17 +1,17 @@
 import PDFFile from '@/templates/pdf/spo'
+import AppLayout from '@/components/layouts/app';
+import SPOHtml from '@/templates/pdf/spo-html';
+
 import { createRoot } from 'react-dom/client'
 import { PDFViewer } from '@react-pdf/renderer';
 import { ReactElement, useEffect, useState } from 'react';
-import AppLayout from '@/components/layouts/app';
 import { useRouter } from 'next/router';
 import { getSession } from 'next-auth/react';
-import SPOHtml from '@/templates/pdf/spo-html';
 
-
-const RenderPage = () => {
-  const router = useRouter()
-  const { nomor } = router.query
-  const [spoDetail, setSpoDetail] = useState<any>(null)
+const RenderPage = ({ data, isMobile }: any) => {
+  // const router = useRouter()
+  // const { nomor } = router.query
+  // const [spoDetail, setSpoDetail] = useState<any>(null)
 
   // useEffect(() => {
   //   if (spoDetail) {
@@ -21,30 +21,44 @@ const RenderPage = () => {
   //   }
   // }, [spoDetail])
 
-  useEffect(() => {
-    const getSpoDetail = async () => {
-      const session = await getSession()
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/berkas/spo/show?nomor=${nomor}`, {
-        method: 'GET',
-        headers: {
-          'Authorization': `Bearer ${session?.rsiap?.access_token}`
-        }
-      })
-
-      const data = await res.json()
-      if (data.success) {
-        setSpoDetail(data.data)
-      }
-    }
-
-    getSpoDetail()
-  }, [nomor])
-
   return (
-    <div className="h-full flex items-center">
-      <SPOHtml data={spoDetail} />
+    <div className="h-full flex items-center px-4 py-5">
+      <SPOHtml data={data} isMobile={isMobile} />
     </div>
   )
+}
+
+export async function getServerSideProps(context: any) {
+  // fetch spo by nomor - spo/show?nomor
+  const session = await getSession(context)
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/berkas/spo/show?nomor=${context.query.nomor}`, {
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${session?.rsiap?.access_token}`
+    }
+  })
+
+  const UA = context.req.headers['user-agent'];
+  const isMobile = Boolean(UA.match(
+    /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i
+  ))
+
+  const data = await res.json()
+  if (data.success) {
+    return {
+      props: {
+        data: data.data,
+        isMobile
+      }
+    }
+  } else {
+    return {
+      props: {
+        data: null,
+        isMobile
+      }
+    }
+  }
 }
 
 // const PDFnya = (data: any) => (
@@ -53,10 +67,10 @@ const RenderPage = () => {
 //   </PDFViewer>
 // )
 
-RenderPage.getLayout = function getLayout(page: ReactElement) {
-  return (
-    <AppLayout>{page}</AppLayout>
-  )
-}
+// RenderPage.getLayout = function getLayout(page: ReactElement) {
+//   return (
+//     <AppLayout>{page}</AppLayout>
+//   )
+// }
 
 export default RenderPage
